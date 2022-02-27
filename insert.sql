e1182c6640abab8a865d7a0b97e08266df6cea04
Noxxxxious <75917724+Noxxxxious@users.noreply.github.com>
2022-02-27 22:31:14 +0100

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,157 @@
+INSERT INTO KONSTELACJE VALUES
+	('Cefeusz', 22, 70),
+	('Kasjopeja', 1, 60),
+	('Ma³a NiedŸwiedzica', 15, 75),
+	('Smok', 17, 65),
+	('Wielka NiedŸwiedzica', 11, 55),
+	('Lis', 20, 25),
+	('Hydra', 10, -20),
+	('Orion', 5.5, 0),
+	('Jednoro¿ec', 7, -5),
+	('Rak', 8.5, 20),
+	('Centaur', 13, -50),
+	('Orze³', 19.5, 3),
+	('Strzelec', 19, -25),
+	('Wodnik', 22.5, -10),
+	('£abêdŸ', 20.5, 40),
+	('Andromeda', 1, 40),
+	('W¹¿', 15.5, 10);
+
+INSERT INTO CIALA_NIEBIESKIE VALUES
+	('Ziemia', 5.97219E24, 6371, 5513, 9.81, 1.47E11, 1.52E11, NULL, NULL, 4.543E9, NULL),
+	('Wenus', 4.867E24, 6051.8, 5243, 8.87, 1.074E8, 1.089E8, NULL, NULL, 4.503E9, NULL),
+	('Merkury', 3.3011E23, 2439.7, 5427, 3.7, 4.6E7, 6.982E8, NULL, NULL, 4.503E9, NULL),
+	('Mars', 6.4171E23, 3389.5, 3933, 3.71, 2.0662E11, 2.4923E11, NULL, NULL, 4.503E9, NULL),
+	('Jowisz', 1.89819E27, 69911, 1326, 24.76, 7.4052E11, 78.1662E11, NULL, NULL, 4.603E9, NULL),
+	('Saturn', 5.6834E26, 58323, 687, 10.44, 1.35255E12, 1.5145E12, NULL, NULL, 4.503E9, NULL),
+	('Uran', 8.6812E25, 25362, 1271, 8.87, 2.7413E12, 3E12, NULL, NULL, 4.468E9, NULL),
+	('Neptun', 1.02413E26, 24622, 1638, 11.15, 4.44E12, 4.545E12, NULL, NULL, 4.503E9, NULL),
+	('Pluton', 1.303E22, 1188, 1880, 4.44E12, 7.32E12, 0.62, NULL, NULL, 4.6E9, NULL),
+	('TRAPPIST-1e', 3.70264E24, 6249.9, 5650, NULL, 4.21E10, 4.13E10, NULL, NULL, 7.605E9, 39),
+	('Proxima Centauri b', 7.5834E24, 8282.3, 5733, NULL, 7.275E9, 7.315E9, 14.5, -62, NULL, 4.24),
+	('Kepler-452b', 6.139E24, 9556.5, 5723, 10.12, 1.5427E11, 1.5667E11, NULL, NULL, 6E9, 1400),
+	('Ksiê¿yc', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4.53E9, NULL),
+	('Europa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4.503E9, NULL),
+	('Fobos', NULL, 11.267, NULL, NULL, NULL, NULL, NULL, NULL, 2.7E9, NULL),
+	('Deimos', NULL, 6.2, NULL, NULL, NULL, NULL, NULL, NULL, 1.6E9, NULL),
+	('Tytan', NULL, 2574.7, NULL, NULL, NULL, NULL, NULL, NULL, 4.003E9, NULL),
+	('Enceladus', NULL, 252.1, NULL, NULL, NULL, NULL, NULL, NULL, 1E9, NULL),
+	('Umbriel', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3.512E9, NULL),
+	('S³oñce', 1.98855E30, 696342, 1408, 274, NULL, NULL, NULL, NULL, 4.603E9, NULL),
+	('Proxima Centauri', 427E29, 107375, 5680, NULL, NULL, NULL, 14.5, -62, 4.85E9, 4.24),
+	('Eta Carinae', 2.5281E32, 1.67E8, NULL, NULL, NULL, NULL, 10.75, -59.57, 3E6, 7500),
+	('TRAPPIST-1', 1.6306E29, 81472, 5030, 1.68, NULL, NULL, 23, -5, 7.605E9, 39),
+	('Kepler-452', 2.06212E30, 772939, 1394, 274, NULL, NULL, NULL, NULL, 6.1E9, 1400),
+	('PSR B1919+21', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('PSR B1913+16', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Vela Pulsar', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('PSR B1937+21', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Droga Mleczna', 1.4E42, NULL, NULL, NULL, NULL, NULL, 21.5, 21.55, 1.351E10 , NULL),
+	('Andromeda', 1.4E42, NULL, NULL, NULL, NULL, NULL, 0.75, 41, 1.001E10 , 2.53E6),
+	('NGC 5204', 1.72E42, NULL, NULL, NULL, NULL, NULL, 13.5, 58.5, 6.9E10, 14.5E6),
+	('NGC 3923', 1.24E42, NULL, NULL, NULL, NULL, NULL, 12, -29, 8.3E10, 71E6),
+	('Hoags Object', 1.35E42, NULL, NULL, NULL, NULL, NULL, 21.5, 21.55, 6.5E10, 612E6),
+	('Markarian 205', 5.78E41, NULL, NULL, NULL, NULL, NULL, 12.5, 75, 7.1E10, 23E6),
+	('Czarna dziura 1', 7.42E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Czarna dziura 2', 7.93E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Czarna dziura 3', 12.51E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Czarna dziura 4', 8.44E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Czarna dziura 5', 5.81E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Czarna dziura 6', 5.22E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Czarna dziura 7', 6.73E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Czarna dziura 8', 9E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	('Czarna dziura 9', 10.24E30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO GALAKTYKI VALUES
+	('Droga Mleczna', 'SBbc', 0, 20.8, NULL),
+	('Andromeda', 'SA(s)b', 0, 20, 'Andromeda'),
+	('NGC 5204', 'SA(s)m', 0, 21, 'Wielka NiedŸwiedzica'),
+	('NGC 3923', 'E4-5', 0, 22, 'Hydra'),
+	('Hoags Object', '(RP)E0', 0, 16.2, 'W¹¿'),
+	('Markarian 205', 'Kwazar', 1, 15.24, 'Smok');
+
+INSERT INTO GWIAZDY VALUES
+	('S³oñce', '¯ó³ty karze³', 'G2 V', 1.46, NULL, NULL, 'Droga Mleczna'),
+	('Proxima Centauri', 'Czerwony karze³', 'M5,5', 0.00073, NULL, 'Centaur', 'Droga Mleczna'),
+	('Eta Carinae', 'S Doradus', 'O', 6.21, NULL, NULL, 'Droga Mleczna'),
+	('TRAPPIST-1', 'Czerwony karze³', 'M8V', 0.00076, NULL, 'Wodnik', 'Droga Mleczna'),
+	('Kepler-452', '¯ó³ty karze³', 'G', 1.51, NULL, '£abêdŸ', 'Droga Mleczna'),
+	('PSR B1919+21', 'Pulsar', 'P',  0.0047, NULL, 'Lis', NULL),
+	('PSR B1913+16', 'Pulsar', 'P',  0.002, NULL, 'Orze³', NULL),
+	('Vela Pulsar', 'Pulsar', 'P',  0.051, NULL, NULL, NULL),
+	('PSR B1937+21', 'Pulsar', 'P',  0.013, NULL, 'Lis', NULL);
+
+INSERT INTO PULSARY VALUES
+	('PSR B1919+21', 0.04, 747.775),
+	('PSR B1913+16', 0.059, 15351),
+	('Vela Pulsar', 0.089, NULL),
+	('PSR B1937+21', 1.557, 641.930);
+
+INSERT INTO PLANETY VALUES
+	('Ziemia', 'Skalista', 23.4393, 23.9345, 365.2563, 'S³oñce'),
+	('Wenus', 'Skalista', 2.64, 5832.6, 224.701, 'S³oñce'),
+	('Merkury', 'Skalista', 0.034, 1392.6458, 87.969, 'S³oñce'),
+	('Mars', 'Skalista', 25.19, 24.6229, 686.980, 'S³oñce'),
+	('Jowisz', 'Gazowa', 3.13, 9.9250, 4332.589, 'S³oñce'),
+	('Saturn', 'Gazowa', 26.73, 10.656, 10759.22, 'S³oñce'),
+	('Uran', 'Gazowa', 82.23, 17.24, 30685.4, 'S³oñce'),
+	('Neptun', 'Gazowa', 28.32, 16.11, 60189, 'S³oñce'),
+	('Pluton', 'Skalista', 57.47, 153.2928, 90498, 'S³oñce'),
+	('TRAPPIST-1e', 'Skalista', 19.86, NULL, 373.922, 'TRAPPIST-1'),
+	('Proxima Centauri b', 'Skalista', 14.21, NULL, 221.753, 'Proxima Centauri'),
+	('Kepler-452b', 'Skalista', 9.71, NULL, 385, 'Kepler-452');
+
+INSERT INTO KSIEZYCE VALUES
+	('Ksiê¿yc', 1.5, 640.38, 27.3, 'Ziemia'),
+	('Europa', 0.466, 83.23, 3.551, 'Jowisz'),
+	('Fobos', 26.04, 7.453, 0.318, 'Mars'),
+	('Deimos', 27.58, 29.57, 1.262, 'Mars'),
+	('Tytan', 0.306, 373.73 , 15.945, 'Saturn'),
+	('Enceladus', 0.009, 32.11, 1.37, 'Saturn'),
+	('Umbriel', 0.128, 97.131, 4.144, 'Uran');
+
+INSERT INTO CZARNE_DZIURY VALUES
+	('Czarna dziura 1', 0, 0, 0, NULL, 'Droga Mleczna', NULL),
+	('Czarna dziura 2', 0, 0, 1, 'Andromeda', 'Andromeda', NULL),
+	('Czarna dziura 3', 0, 1, 0, NULL, 'Droga Mleczna', 'Czarna dziura 9'),
+	('Czarna dziura 4', 0, 1, 1, 'Andromeda', 'Andromeda', NULL),
+	('Czarna dziura 5', 1, 0, 0, 'Wielka NiedŸwiedzica', 'NGC 5204', NULL),
+	('Czarna dziura 6', 1, 0, 1, 'W¹¿', 'Hoags Object', NULL),
+	('Czarna dziura 7', 1, 1, 0, NULL, 'Hoags Object', NULL),
+	('Czarna dziura 8', 1, 1, 1, 'Hydra', 'NGC 3923', NULL),
+	('Czarna dziura 9', 1, 0, 1, NULL, 'Droga Mleczna', 'Czarna dziura 3');
+
+INSERT INTO TELESKOPY VALUES
+	('Hubble', 'Kosmiczny', 'NASA, ESA, STScl', '1990-04-24'),
+	('Kepler', 'Kosmiczny', 'NASA', '2009-03-07'),
+	('Webb', 'Kosmiczny', 'NASA, ESA, CSA, STScl', '2021-12-24'),
+	('Compton', 'Kosmiczny', 'NASA', '1991-04-05'),
+	('Spitzer', 'Kosmiczny', 'NASA', '2003-08-25'),
+	('Chandra', 'Kosmiczny', 'NASA', '1999-07-23'),
+	('Planck', 'Kosmiczny', 'ESA',' 2013-10-23'),
+	('Teleskop nr1', 'Naziemny', 'Obserwatorium ETI',' 2018-03-12'),
+	('Teleskop nr2', 'Naziemny', 'Obserwatorium ETI',' 2019-11-05'),
+	('Teleskop nr3', 'Naziemny', 'Obserwatorium ETI',' 2020-07-23');
+
+INSERT INTO OBSERWACJE VALUES
+	('OB-0000000001', '2019-01-01', 'Miko³aj Kopernik', 'Teleskop nr1', 'Ksiê¿yc'),
+	('OB-0000000002', '2019-01-02', 'Miko³aj Kopernik', 'Teleskop nr1', 'Mars'),
+	('OB-0000000003', '2019-01-03', 'Miko³aj Kopernik', 'Teleskop nr1', 'Neptun'),
+	('OB-0000000004', '2020-02-01', 'Galileusz', 'Teleskop nr2', 'Jowisz'),
+	('OB-0000000005', '2020-02-02', 'Galileusz', 'Teleskop nr2', 'Fobos'),
+	('OB-0000000006', '2020-12-24', 'Johannes Kepler', 'Teleskop nr2', 'Andromeda'),
+	('OB-0000000007', '2020-12-25', 'Johannes Kepler', 'Teleskop nr2', 'Pluton'),
+	('OB-0000000008', '2020-12-26', 'Johannes Kepler', 'Teleskop nr2', 'Proxima Centauri'),
+	('OB-0000000009', '2021-04-13', 'Isaac Newton', 'Teleskop nr3', 'Saturn'),
+	('OB-0000000010', '2021-04-14', 'Isaac Newton', 'Teleskop nr3', 'TRAPPIST-1'),
+	('OB-0000000011', '2021-04-15', 'Isaac Newton', 'Teleskop nr3', 'Deimos'),
+	('OB-0000000012', '2021-11-20', 'Albert Einstein', 'Teleskop nr3', 'Czarna dziura 7');
+
+INSERT INTO ODKRYCIA VALUES
+	('OD-0000000001', '2019-01-03', 'Miko³aj Kopernik', 'Teleskop nr1', 'Pluton'),
+	('OD-0000000002', '2020-02-01', 'Galileusz', 'Teleskop nr2', 'Europa'),
+	('OD-0000000003', '2020-02-02', 'Galileusz', 'Teleskop nr2', 'Deimos'),
+	('OD-0000000004', '2020-12-26', 'Johannes Kepler', 'Teleskop nr2', 'Proxima Centauri b'),
+	('OD-0000000005', '2021-04-13', 'Isaac Newton', 'Teleskop nr3', 'Enceladus'),
+	('OD-0000000006', '2021-04-14', 'Isaac Newton', 'Teleskop nr3', 'TRAPPIST-1e'),
+	('OD-0000000007', '2021-11-20', 'Albert Einstein', 'Teleskop nr3', 'Czarna dziura 3');
